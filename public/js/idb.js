0966ae8065f7cf691d01ac02d881c6dc76c81cd1
spaghettimedic <dsteiner247@gmail.com>
2022-02-19 14:49:38 -0500

    fixed indexedDB bug (spelling error in idb.js), offline functionality tested and working

--- public/js/idb.js
+++ public/js/idb.js
@@ -1,5 +1,4 @@
 let db;
-
 const request = indexedDB.open('budget_tracker', 1);
 
 request.onupgradeneeded = function(event) {
@@ -7,32 +6,12 @@ request.onupgradeneeded = function(event) {
   db.createObjectStore('new_trans', { autoIncrement: true });
 };
 
-request.onsuccess = function(event) {
-  db = event.target.result;
-
-  if (navigator.onLine) {
-    uploadTrans();
-  }
-};
-
-request.onerror = function(event) {
-  console.log(event.target.errorCode);
-};
-
-// this function will execute if there's no internet connection and a transaction is submitted by the client
-function saveRecord(record) {
-  const transaction = db.transaction(['new_trans'], 'readwrite');
-  const transObjectStore = transaction.objectStore('new_trans');
-
-  transObjectStore.add(record);
-};
-
 function uploadTrans() {
   const transaction = db.transaction(['new_trans'], 'readwrite');
   const transObjectStore = transaction.objectStore('new_trans');
   const getAll = transObjectStore.getAll();
 
-  getAll.onscuccess = function() {
+  getAll.onsuccess = function() {
     if (getAll.result.length > 0) {
       fetch('/api/transaction', {
         method: 'POST',
@@ -56,6 +35,26 @@ function uploadTrans() {
       .catch(err => console.log(err));
     }
   }
+};
+
+request.onsuccess = function(event) {
+  db = event.target.result;
+
+  if (navigator.onLine) {
+    uploadTrans();
+  }
+};
+
+request.onerror = function(event) {
+  console.log(event.target.errorCode);
+};
+
+// this function will execute if there's no internet connection and a transaction is submitted by the client
+function saveRecord(record) {
+  const transaction = db.transaction(['new_trans'], 'readwrite');
+  const transObjectStore = transaction.objectStore('new_trans');
+
+  transObjectStore.add(record);
 };
 
 window.addEventListener('online', uploadTrans);
