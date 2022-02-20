f9fe0c94c2bf7209c88cadc46c915917b5ca3fd6
spaghettimedic <dsteiner247@gmail.com>
2022-02-20 12:29:47 -0500

    changed literally nothing and it just works now... :|

--- public/service-worker.js
+++ public/service-worker.js
@@ -39,7 +39,7 @@ self.addEventListener('activate', function(e) {
 });
 
 self.addEventListener('fetch', function(e) {
-  console.log('fetch request: ' + e.request.url)
+  console.log('fetch request: ' + e.request.url);
   e.respondWith(
     caches.match(e.request)
     .then(function(request) {
